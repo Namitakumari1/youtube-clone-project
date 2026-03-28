44ba3475c2d6f7412825ddb3c6105c01b7667fa5
Namita Kumari <namitadhn001@gmail.com>
2026-03-28 22:02:07 +0530

    Added comments models

---
+++ Youtube-Backend/models/Comment.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  video: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Video",
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
