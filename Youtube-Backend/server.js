3bf93abe4ef8e609ac021af71862a407e0d21c5a
Namita Kumari <namitadhn001@gmail.com>
2026-04-18 18:08:46 +0530

    update backend

--- Youtube-Backend/server.js
+++ Youtube-Backend/server.js
@@ -1,47 +1,3 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from "dotenv";
-// import { userRoutes } from "./routes/user.route.js";
-// import { channelRoutes } from './routes/channel.route.js';
-// import { videoRoutes } from './routes/video.route.js';
-// import { commentRoutes } from './routes/comment.route.js';
-// import cors from "cors";
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors({
-//   origin: "*",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true
-// }));
-// app.use(express.json());
-
-// const port = 3000;
-// app.listen(port, () => {
-//     console.log("Server is running on port 3000");
-// });
-
-// const connectwithdatabase = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI);
-//         console.log("Database connection is successful");
-//     } catch (err) {
-//         console.log(err);
-//     }
-// };
-// connectwithdatabase();
-
-// //routes function
-// userRoutes(app);
-
-// channelRoutes(app);
-
-// videoRoutes(app);
-
-// commentRoutes(app);
-
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
@@ -57,10 +13,12 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:5174",
+  origin: [
+    "http://localhost:5174",
+    "https://youtube-clone-project-gamma.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
-
 app.use(express.json());
 
 
