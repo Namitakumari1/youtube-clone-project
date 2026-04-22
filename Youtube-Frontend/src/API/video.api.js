71c89edc4368be13caa9dc868c089ffe7e3bb21b
Namita Kumari <namitadhn001@gmail.com>
2026-04-22 07:52:02 +0530

    Updated user.api.js, video.api.js, .env, server.js

--- Youtube-Frontend/src/API/video.api.js
+++ Youtube-Frontend/src/API/video.api.js
@@ -2,12 +2,12 @@ import API from "./axios";
 
 // Fetch all videos from backend
 export const getVideos = async () => {
-  const res = await API.get("/video");
+  const res = await API.get("/api/video");
   return res.data;
 };
 
 // Fetch a single video using its unique id
 export const getVideoById = async (id) => {
-  const res = await API.get(`/video/${id}`);
+  const res = await API.get(`/api/video/${id}`);
   return res.data;
 };
