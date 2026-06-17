e8b1fd4a38dc301717dbaeefcf6e006525cf49e3
Namita Kumari <namitadhn001@gmail.com>
2026-06-17 12:36:55 +0530

    Updated server.js and vite.config.js files

--- Youtube-Frontend/vite.config.js
+++ Youtube-Frontend/vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,      // Tells Vite: "Always try to use 5173"
+    strictPort: true // Tells Vite: "If 5173 is busy, FAIL. Do not jump to 5174."
+  }
 })
